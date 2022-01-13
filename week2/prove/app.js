2ba346358f43dada48121bc1fc104ddc45ee6646
Kayleigh Hansen <71479913+kayleighhansen@users.noreply.github.com>
2022-01-12 22:55:16 -0700

    ...

--- week2/prove/app.js
+++ week2/prove/app.js
@@ -1,4 +1,10 @@
 const http = require('http');
+const express = require('express');
+
+const app = express();
+app.use((req, res, next) => {
+    console.log('In the middleware');
+});
 
 console.log(routes.someText);
 
