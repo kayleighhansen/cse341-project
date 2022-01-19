c41cfaed09d9b944b60d7b62dae8300b13d63f2e
Kayleigh Hansen <71479913+kayleighhansen@users.noreply.github.com>
2022-01-19 13:23:48 -0700

    ...

--- week3/prove/routes/admin.js
+++ week3/prove/routes/admin.js
@@ -4,11 +4,13 @@ const express = require('express');
 
 const router = express.Router();
 
-const productsController = require('../controllers/product');
+const adminController = require('../controllers/admin');
 
 
-router.get('/add-product', productsController.getAddProduct);
+router.get('/add-product', adminController.getAddProduct);
 
-router.post('/add-product', productsController.postAddProduct);
+router.get('/products', adminController.getProducts);
+
+router.post('/add-product', adminController.postAddProduct);
 
 module.exports = router;
